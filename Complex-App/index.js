103148d18150f27fe4173fa10be5065717e5347f
arno1408 <arno.thelastmilestone@gmail.com>
2025-08-01 21:23:37 +0530

    complex-app build

--- Complex-App/index.js
+++ Complex-App/index.js
@@ -1,12 +1,33 @@
-let http = require("http");
+let express = require("express");
 
-let app = http.createServer(function (req, res) {
-  if (req.url == "/") {
-    res.end("Hello and welcome to our website");
-  }
-  if (req.url == "/about") {
-    res.end("Thank you for the interest in our company ");
+let app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", function (req, res) {
+  res.send(`
+    <form action="/answer" method="POST">
+    <p>What color is the sky on a clear and sunny day?</p>
+    <input type="text" name="skyColor" autocomplete="off" />
+    <button>Submit Answer</button>
+    </form>
+    `);
+});
+
+app.post("/answer", function (req, res) {
+  if (req.body.skyColor == "blue") {
+    res.send(`<p>That's the correct answer!</p> 
+      <a href="/">Back to home page!</a>
+      `);
+  } else {
+    res.send(`<p>That's incorrect!</p> 
+      <a href="/">Back to home page!</a>
+      `);
   }
 });
+
+// app.get("/answer", function (req, res) {
+//   res.send("Are you lost?");
+// });
 
 app.listen(3000);
